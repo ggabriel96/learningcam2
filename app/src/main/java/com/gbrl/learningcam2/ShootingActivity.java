e9cc0f85bcb0e5064825747074fcbe75280b9006
Gabriel Galli <g7.galli96@gmail.com>
2016-10-18 22:17:48 -0200

    Created project

---
+++ app/src/main/java/com/gbrl/learningcam2/ShootingActivity.java
@@ -0,0 +1,12 @@
+package com.gbrl.learningcam2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShootingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
